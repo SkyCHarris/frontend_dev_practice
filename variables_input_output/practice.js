ef9f1fbe225ffc8bf0b208b2ce31f4aef26bb822
Skyler Harris <skyler.iqeq@gmail.com>
2021-03-05 10:55:20 +0800

    First solution, using template literals, length method, etc.

--- variables_input_output/practice.js
+++ variables_input_output/practice.js
@@ -2,16 +2,27 @@
 
 // 2. In this JavaScript file, add a prompt dialog to capture input from the user and store it in a variable
 
+let firstName = prompt("Please enter your first name.");
+
 // 3. Add a second a prompt dialog to capture input from the user and store it in a second variable
+
+let lastName = prompt("Please enter your last name.");
 
 // 4. Create a third variable and which combines an uppercase version values in the two other variables separated by a space. For example, if the first two variables contain "sally" and "forth", this third variable should contain the string value "SALLY FORTH"
 
+let fullName = `${firstName.toUpperCase()} ${lastName.toUpperCase()}`;
+
+console.log(fullName);
+
 // 5. Create a fourth variable to store a number. The number should be the total number of characters in the third variable. 
+
+let nameString = fullName.length;
+
+console.log(nameString);
 
 // 6. Add an alert dialog box that says "The string '[insert value of third variable here]' is X number of characters long." For example, if the third variable contained the string "SALLY FORTH" then the alert dialog should says "The string 'SALLY FORTH' is 11 characters long."
 
-
-
+alert(`The string ${fullName} is ${nameString} characters long.`);
 
 
 
