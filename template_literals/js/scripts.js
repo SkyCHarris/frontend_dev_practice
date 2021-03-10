bda80dbe86266c34a49076bf752acbc23212c888
Skyler Harris <skyler.iqeq@gmail.com>
2021-03-10 16:43:57 +0800

    Template Literal refactor, first solution attempt

--- template_literals/js/scripts.js
+++ template_literals/js/scripts.js
@@ -9,22 +9,39 @@ const mars = {
   facts: 'Mars has the largest dust storms in the solar system. They can last for months and cover the entire planet. On Mars the Sun appears about half the size as it does on Earth.'
 };
 
+// function createPlanetHTML(planet) {
+//  return '<div class="card">' +
+//           '<div>'+
+//             '<img src="img/' + planet.name + '.jpg" alt="' + planet.name + '">' +
+//           '</div>' +
+//           '<h2>' + planet.name + '</h2>' +
+//           '<p>' + planet.description + '</p>' +
+//           '<h3>Planet Profile</h3>' +
+//           '<ul>' +
+//             '<li><strong>Diameter: </strong>' + planet.diameter + '</li>' +
+//             '<li><strong>Moons: </strong>' + planet.moons + '</li>' +
+//             '<li><strong>Temperature: </strong>' + planet.temp + '</li>' +
+//             '<li><strong>Orbit Period: </strong>' + planet.orbitDays + ' days ' + '(' + planet.orbitYears + ' years)' + '</li>' +
+//           '</ul>' +
+//           '<p>' + planet.facts + '</p>' +
+//         '</div>';
+
 function createPlanetHTML(planet) {
- return '<div class="card">' +
-          '<div>'+
-            '<img src="img/' + planet.name + '.jpg" alt="' + planet.name + '">' +
-          '</div>' +
-          '<h2>' + planet.name + '</h2>' +
-          '<p>' + planet.description + '</p>' +
-          '<h3>Planet Profile</h3>' +
-          '<ul>' +
-            '<li><strong>Diameter: </strong>' + planet.diameter + '</li>' +
-            '<li><strong>Moons: </strong>' + planet.moons + '</li>' +
-            '<li><strong>Temperature: </strong>' + planet.temp + '</li>' +
-            '<li><strong>Orbit Period: </strong>' + planet.orbitDays + ' days ' + '(' + planet.orbitYears + ' years)' + '</li>' +
-          '</ul>' +
-          '<p>' + planet.facts + '</p>' +
-        '</div>';
+  return `<div class="card">
+            <div>
+              <img src="img/${planet.name}.jpg" alt=" ${planet.name} ">
+            </div>
+            <h2> ${planet.name} </h2>
+            <p> ${planet.description} </p>
+            <h3>Planet Profile</h3>
+            <ul>
+              <li><strong>Diameter: </strong> ${planet.diameter} </li>
+              <li><strong>Moons: </strong> ${planet.moons} </li>
+              <li><strong>Temperature: </strong> ${planet.temp} </li>
+              <li><strong>Orbit Period: </strong> ${planet.orbitDays} days ( ${planet.orbitYears} years) </li>
+            </ul>
+            <p> ${planet.facts} </p>
+          </div>`;
 }
 
 document.querySelector('body').innerHTML = createPlanetHTML(mars);
