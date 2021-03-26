35dff1ab3a833e01dae856f39947b200a576fa5d
Skyler Harris <skyler.iqeq@gmail.com>
2021-03-26 10:13:33 +0800

    Write if statements for num1 and num2 variables and isNan function

--- if_and_else_if_statements/js/math.js
+++ if_and_else_if_statements/js/math.js
@@ -1,16 +1,29 @@
 // declare program variables
+let num1;
+let num2;
+let message;
 
 // announce the program
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
+num1 = prompt("Please type a number");
+if (num1 === '0') {
+    alert("Not a number, please reload and try again.");
+}
 num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
+
+num2 = prompt("Please type another number.");
+
+function checkNum() {
+    if ( isNan(num1) || isNan(num2) ) {
+        alert("At least one of the values you typed is not a number. Please reload and try again.");
+    }
+}
 num2 = parseFloat(num2);
 
 // build an HTML message
-var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 message += num1 + " + " + num2 + " = " + (num1 + num2);
 message += "<br>";
 message += num1 + " * " + num2 + " = " + (num1 * num2);
