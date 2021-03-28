4ff75e8ec56cbd3f208df8d8a8650826064d1e7d
Skyler Harris <skyler.iqeq@gmail.com>
2021-03-28 17:01:44 +0800

    Jay's solution

--- if_and_else_if_statements/js/math.js
+++ if_and_else_if_statements/js/math.js
@@ -9,14 +9,13 @@ alert("Let's do some math!");
 // collect numeric input
 num1 = prompt("Please type a number");
 num1 = parseFloat(num1);
-if (num1 === '0') {
-    alert("Not a number, please reload and try again.");
-}
-
 num2 = prompt("Please type another number.");
 num2 = parseFloat(num2);
-if ( isNaN(num1) || isNaN(num2) ) {
-    alert("At least one of the values you typed is not a number. Please reload and try again.");
+
+if (num2 === 0) {
+    alert("Not a number, please reload and try again.");
+} else if (isNaN(num1) || (isNaN(num2))) {
+    alert("At least one of the values you typed is not a number. Reload and try again.");
 } else {
     message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
     message += num1 + " + " + num2 + " = " + (num1 + num2);
@@ -28,6 +27,8 @@ if ( isNaN(num1) || isNaN(num2) ) {
     message += num1 + " - " + num2 + " = " + (num1 - num2);   
     document.write(message);
 }
+
+
 
 // build an HTML message
 
